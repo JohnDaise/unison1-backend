3c09642df9b22c58b72c089fea2d92f987da0099
JohnDaise <jdaise@gmail.com>
2018-10-18 15:48:14 -0400

    backend setup for user events

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,2 +1,47 @@
 class UserEventsController < ApplicationController
+  skip_before_action :authenticate, only: [:index]
+
+  def index
+    render json: UserEvent.all
+  end
+
+  def show
+    render json: UserEvent.find(params[:id])
+  end
+
+  def create
+    render json: UserEvent.create(user_event_params)
+  end
+
+  def update
+    @user_event.update(user_event_params)
+    if @user_event.save
+      render json: @user_event, status: :accepted
+    else
+      render json: { errors: @user_event.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+
+  def destroy
+    render json: UserEvent.find(params[:id]).destroy
+  end
+
+
+
+    private
+
+      def user_event_params
+        params.require(:user_event).permit(:user_id, :event_id)
+      end
+
+      def find_user_event
+        @user_event = UserEvent.find(params[:id])
+      end
+
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       resources :users
       resources :events
+      resources :user_events
       post '/login', to: 'auth#login'
       get '/profile', to: 'users#show'
 end
